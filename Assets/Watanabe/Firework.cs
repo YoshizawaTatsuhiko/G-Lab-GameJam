5f73a9afd87814ddee3d612d45f7eefb4c8b37b5
Akira Watanabe <a0323w@gmail.com>
2023-07-17 19:11:48 +0900

    [update] Collider修正

--- Assets/Watanabe/Firework.cs
+++ Assets/Watanabe/Firework.cs
@@ -4,17 +4,19 @@ public class Firework : MonoBehaviour
 {
     [SerializeField]
     private Firing _firing = new();
-    [SerializeField]
+
     private int _attackValue = 1;
+    private CircleCollider2D _circleCollider = default;
 
     private void Start()
     {
         _firing.Init(transform);
+        _circleCollider = GetComponent<CircleCollider2D>();
     }
 
-    public void ShowFirework()
+    public void ShowFirework(float radius)
     {
-        _firing.Explode(gameObject);
+        _firing.Explode(gameObject, radius);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -22,6 +24,27 @@ public class Firework : MonoBehaviour
         if (collision.gameObject.TryGetComponent(out EnemyController enemy))
         {
             enemy.gameObject.TryGetComponent(out LifeManager life);
+
+            if (_circleCollider.radius <= 1)
+            {
+                _attackValue = 2;
+            }
+            else if (_circleCollider.radius <= 2)
+            {
+                _attackValue = 4;
+            }
+            else if (_circleCollider.radius <= 3)
+            {
+                _attackValue = 5;
+            }
+            else if (_circleCollider.radius <= 4)
+            {
+                _attackValue = 6;
+            }
+            else
+            {
+                _attackValue = 10;
+            }
             life.ReduceLife(_attackValue);
         }
     }
@@ -30,7 +53,7 @@ public class Firework : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, GetComponent<CircleCollider2D>().radius);
+        Gizmos.DrawWireSphere(transform.position, _circleCollider.radius);
     }
 #endif
 }
