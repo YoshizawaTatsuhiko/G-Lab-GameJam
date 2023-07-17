8ca521e403aef0ace70f1e655b6aded96b154255
Akira Watanabe <a0323w@gmail.com>
2023-07-17 17:12:19 +0900

    [update] 現状進捗

--- Assets/Watanabe/Firework.cs
+++ Assets/Watanabe/Firework.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 public class Firework : MonoBehaviour
 {
     [SerializeField]
-    private Sprite[] _fireworkSprites = default;
+    private Firing _firing = new();
     [SerializeField]
     private LifeManager _lifeManager = default;
+    [SerializeField]
+    private int _attackValue = 1;
 
     private SpriteRenderer _renderer = default;
 
     private void Start()
     {
+        _firing.Init(transform);
         _renderer = GetComponent<SpriteRenderer>();
     }
 
     public void ShowFirework(int scale)
     {
-        _renderer.sprite = _fireworkSprites[scale - 1];
-
         _renderer.transform.localScale = Vector3.zero;
         _renderer.transform.DOScale(Vector3.one * scale, 1.5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out EnemyController enemy))
+        {
+            enemy.gameObject.TryGetComponent(out LifeManager life);
+            life.ReduceLife(_attackValue);
+        }
     }
 }
---
+++ Assets/Watanabe/Firing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+[System.Serializable]
+public class Firing
+{
+    [SerializeField]
+    private GameObject _explodePrefab = default;
+    [SerializeField]
+    private Vector3 _upOffset = Vector3.up;
+
+    private Transform _transform = default;
+    private Vector3 _startPos = Vector3.zero;
+
+    public GameObject ExplodePrefab => _explodePrefab;
+
+    public void Init(Transform transform)
+    {
+        _transform = transform;
+        _startPos = transform.position;
+    }
+
+    public void Explode()
+    {
+        var sequence = DOTween.Sequence();
+
+        sequence.Append(_transform.DOMove(_startPos + _upOffset, 1f));
+    }
+}
