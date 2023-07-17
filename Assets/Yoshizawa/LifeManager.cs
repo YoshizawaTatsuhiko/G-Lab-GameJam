d2a8c49aae9b5ca339b1b35db2141c3634ded34f
tatsupiko <identity2022@outlook.jp>
2023-07-17 14:26:24 +0900

    Lifeを管理するクラスを作成した。

---
+++ Assets/Yoshizawa/LifeManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 日本語対応
+public class LifeManager : MonoBehaviour
+{
+    [SerializeField, Range(1, 10)] private int _life = 1;
+
+    public void ReduceLife(int damage)
+    {
+        _life -= damage;
+
+        if (_life <= 0) Destroy(gameObject);
+    }
+}
