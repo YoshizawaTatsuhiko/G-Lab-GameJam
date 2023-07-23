c3a86f97999f234bf4eaa7c9490b5aa6dd01c4ba
tatsupiko <identity2022@outlook.jp>
2023-07-23 23:17:00 +0900

    GameManagerをシングルトン化し、ゲーム終了時に倒すフラグを追加した。

--- Assets/Yoshizawa/GameManager.cs
+++ Assets/Yoshizawa/GameManager.cs
@@ -6,31 +6,43 @@ using UnityEngine.UI;
 // 日本語対応
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance => _instance;
+    public bool IsPause { get; private set; } = false;
+
     [SerializeField] private LifeManager _life = null;
     [SerializeField] private Text _lifeText = null;
     [SerializeField] private GameObject _resultPanel = null;
+    private static GameManager _instance = null;
     private Text _resultText = null;
     private float _survivalTime = 0f;
 
+    private void Awake()
+    {
+        if (!_instance) _instance = this;
+        else Destroy(this);
+    }
+
     private void Start()
     {
+        IsPause = false;
         _resultPanel.SetActive(false);
-
-        if (_resultPanel.TryGetComponent(out Text t)) _resultText = t;
+        _resultText = _resultPanel.GetComponentInChildren<Text>();
     }
 
     private void Update()
     {
-        if (!_life || !_resultPanel) throw new System.Exception("assignされていない項目があります。");
-
-        if(_lifeText) _lifeText.text = $"Life : {_life.Life}";
-
-        if (_life.Life > 0) _survivalTime += Time.deltaTime;
+        if (!_life)
+        {
+            _lifeText.text = "Life : 0";
+            _resultPanel.SetActive(true);
+            if (_resultText) _resultText.text = $"貴方は[{_survivalTime.ToString("F2")}秒]守れた！";
+            IsPause = true;
+        }
         else
         {
-            _resultPanel.SetActive(true);
-
-            if (_resultText) _resultText.text = $"貴方は「{_survivalTime.ToString("F2")}秒間」\n守りきれた！";
+            _lifeText.text = $"Life : {_life.Life}";
         }
+
+        if (_life.Life > 0) _survivalTime += Time.deltaTime;
     }
 }
